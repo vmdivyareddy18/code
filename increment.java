7b1b7e08d46e13a76a26859730b60853c96297e6
V.M Divya Reddy <vmdivyareddy18@gmail.com>
2026-08-12 23:41:24 +0530

    7

--- increment.java
+++ increment.java
@@ -1,26 +1,43 @@
 {
-    
-}
-int x = 10;
 
-x = x + 1;
+}int x=10;
 
-System.out.println(x);
-int x = 10;
+x=x+1;
+
+System.out.println(x);int x=10;
 
 x++;
 
-System.out.println(x);
-int x = 10;
+System.out.println(x);int x=10;
 
 x--;
 
-System.out.println(x);
-x++;     // x = x + 1
-x--;     // x = x - 1
+System.out.println(x);x++; // x = x + 1
+x--; // x = x - 1
 
-x += 5;  // x = x + 5
-x -= 5;  // x = x - 5
-x *= 2;  // x = x * 2
-x /= 2;  // x = x / 2
-x %= 3;  // x = x % 3
+x+=5; // x = x + 5
+x-=5; // x = x - 5
+x*=2; // x = x * 2
+x/=2; // x = x / 2
+x%=3; // x = x % 3
+class AssignmentDemo {
+    public static void main(String[] args) {
+
+        int x = 10;
+
+        x += 5;
+        System.out.println(x);
+
+        x -= 3;
+        System.out.println(x);
+
+        x *= 2;
+        System.out.println(x);
+
+        x /= 4;
+        System.out.println(x);
+
+        x %= 3;
+        System.out.println(x);
+    }
+}
