410556249d820f024701c3cdbca4d4cada1f7194
V.M Divya Reddy <vmdivyareddy18@gmail.com>
2026-08-12 23:47:11 +0530

    1

--- increment.java
+++ increment.java
@@ -63,3 +63,25 @@ class AssignmentDemo {
         System.out.println(x);
     }
 }
+
+class AssignmentDemo {
+    public static void main(String[] args) {
+
+        int x = 10;
+
+        x += 5;
+        System.out.println(x);
+
+        x -= 3;
+        System.out.println(x);
+
+        x *= 2;
+        System.out.println(x);
+
+        x /= 4;
+        System.out.println(x);
+
+        x %= 3;
+        System.out.println(x);
+    }
+}
