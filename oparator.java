c85e0571e128056248d0ef726291fa421c3c6a24
V.M Divya Reddy <vmdivyareddy18@gmail.com>
2026-08-12 23:23:16 +0530

    1

---
+++ oparator.java
@@ -0,0 +1,14 @@
+class  {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 3;
+
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+        System.out.println("Remainder: " + (a % b));
+
+    }
+}
