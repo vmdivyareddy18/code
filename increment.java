8e5b6badfc8c3f24ced82607ecfe271e1e3e3a56
V.M Divya Reddy <vmdivyareddy18@gmail.com>
2026-08-13 23:04:00 +0530

    16

--- increment.java
+++ increment.java
@@ -217,5 +217,17 @@ class AssignmentDemo {
         System.out.println("B");
     }else
     {
+        System.out.println("C");
+    }
+    int marks = 80;
+
+    if(marks>=90)
+    {
+        System.out.println("A");
+    }else if(marks>=75)
+    {
+        System.out.println("B");
+    }else
+    {
     System.out.println("C");
 }
