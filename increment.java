064bef5b00398452350526e1fc09685f8a60d3e8
V.M Divya Reddy <vmdivyareddy18@gmail.com>
2026-08-13 23:00:57 +0530

    10

--- increment.java
+++ increment.java
@@ -143,7 +143,19 @@ class AssignmentDemo {
 
     if(age>=18)
     {
-    if (hasID) {
-        System.out.println("Entry Allowed");
+        if (hasID) {
+            System.out.println("Entry Allowed");
+        }
     }
+    int marks = 95;
+
+    if(marks>=90)
+    {
+        System.out.println("Grade A");
+    }else if(marks>=75)
+    {
+        System.out.println("Grade B");
+    }else
+    {
+    System.out.println("Pass");
 }
