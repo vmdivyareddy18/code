3f5a08a03ea2703730956d71761f62cc80ba5074
V.M Divya Reddy <vmdivyareddy18@gmail.com>
2026-08-14 23:21:22 +0530

    1

--- increment.java
+++ increment.java
@@ -188,14 +188,5 @@ class AssignmentDemo {
     }else if(marks>=90)
     {
         System.out.println("Grade A");
-    }
-    int number = 7;
-
-    if(number%2==0)
-    {
-        System.out.println("Even");
-    }else
-    {
-        System.out.println("Odd");
 }
 int number = 7;
