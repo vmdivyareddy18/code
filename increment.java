46a104749f1632fe22021d70d4dc984b61223601
V.M Divya Reddy <vmdivyareddy18@gmail.com>
2026-08-14 23:19:42 +0530

    3

--- increment.java
+++ increment.java
@@ -239,11 +239,5 @@ class AssignmentDemo {
     }else
     {
         System.out.println("Minor");
-    }
-    int age = 20;
-
-    if(age>=18)
-    {
-        System.out.println("Adult");
 }
 int age = 20;
